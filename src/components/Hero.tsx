2c51f91c14142b701c1f670894e76e5d49161d6e
DavidWangwjh <davidwang.wjh@gmail.com>
2024-07-24 23:32:01 -0700

    fixed theme switching and sped up hero text typing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,14 +30,15 @@ const Hero = ({ innerRef }: HeroProps ) => {
             width={300}
             height={300}
             className="rounded-full w-full border-8"
+            priority
         />
       </motion.div>
       <div className='flex flex-col items-center lg:items-start w-full md:w-4/5 lg:w-3/5'>
-        <TypeAnimation className="bold-32 sm:bold-36 md:bold-48 lg:bold-52" sequence={['', 1000, 'I\'M DAVID WANG']} cursor={false} speed={20}/>
-        <TypeAnimation className="regular-16 lg:regular-22 pt-4 pb-2 px-7 lg:px-0 text-center lg:text-start w-full" sequence={['', 2300, 'Software Engineer, Web Developer, and App Developer']} cursor={false} speed={80}/>
-        <TypeAnimation className="max-md:hidden regular-16 lg:regular-18 pb-8 text-center lg:text-start w-full" sequence={['', 3500, 'University of California, San Diego | Computer Science B.S.']} cursor={false} speed={80}/>
-        <TypeAnimation className="md:hidden regular-16 lg:regular-18 pb-8 text-center lg:text-start w-full" sequence={['', 3500, 'UCSD Computer Science B.S.']} cursor={false} speed={80}/>
-        <motion.ul className="flex flex-row gap-4 md:gap-6 w-full justify-center lg:justify-start items-center" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 4, duration: 2.5}}>
+        <TypeAnimation className="bold-32 sm:bold-36 md:bold-48 lg:bold-52" sequence={['', 1000, 'I\'M DAVID WANG']} cursor={false} speed={40}/>
+        <TypeAnimation className="regular-16 lg:regular-22 pt-4 pb-2 px-7 lg:px-0 text-center lg:text-start w-full" sequence={['', 2000, 'Software Engineer, Web Developer, and App Developer']} cursor={false} speed={90}/>
+        <TypeAnimation className="max-md:hidden regular-16 lg:regular-18 pb-8 text-center lg:text-start w-full" sequence={['', 2800, 'University of California, San Diego | Computer Science B.S.']} cursor={false} speed={90}/>
+        <TypeAnimation className="md:hidden regular-16 lg:regular-18 pb-8 text-center lg:text-start w-full" sequence={['', 3000, 'UCSD Computer Science B.S.']} cursor={false} speed={90}/>
+        <motion.ul className="flex flex-row gap-4 md:gap-6 w-full justify-center lg:justify-start items-center" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 3, duration: 2.5}}>
           <Link href='/resume.html' target="_blank" rel="noopener noreferrer nofollow" className='flex items-center bg-black dark:bg-light text-light dark:text-dark py-2.5 px-4 gap-2 rounded-lg text-lg font-semibold hover:scale-110'>
             RESUME
             <LinkArrow className={''}/>
--- src/components/hooks/useThemeSwitcher.tsx
+++ src/components/hooks/useThemeSwitcher.tsx
@@ -1,42 +1,46 @@
-import React, {useEffect, useState} from 'react'
+import { useState, useEffect } from 'react';
 
 const useThemeSwitcher = () => {
-    const preferDarkQuery = '(prefer-color-scheme: dark)'
-    const [mode, setMode] = useState('')
+    const [mode, setMode] = useState<string | null>(null);
 
     useEffect(() => {
-        const mediaQuery = window.matchMedia(preferDarkQuery)
-        const userPref = window.localStorage.getItem('theme')
+        const preferDarkQuery = '(prefers-color-scheme: dark)';
+        const mediaQuery = window.matchMedia(preferDarkQuery);
+        const userPref = window.localStorage.getItem('theme');
 
         const handleChange = () => {
-            let check = ''
+            let check = '';
             if (userPref) {
-                check = userPref
+                check = userPref;
             } else {
-                check = mediaQuery.matches? 'dark' : 'light'
+                check = mediaQuery.matches ? 'dark' : 'light';
             }
             setMode(check);
-            if (check == 'dark'){
-                document.documentElement.classList.add('dark')
+            if (check === 'dark') {
+                document.documentElement.classList.add('dark');
             } else {
-                document.documentElement.classList.remove('dark')
+                document.documentElement.classList.remove('dark');
             }
-        }
-        mediaQuery.addEventListener('change', handleChange)
-        return () => mediaQuery.removeEventListener('change', handleChange)
-    }, [])
+        };
+
+        handleChange();
+        mediaQuery.addEventListener('change', handleChange);
+
+        return () => mediaQuery.removeEventListener('change', handleChange);
+    }, []);
 
     useEffect(() => {
-        if (mode == 'dark'){
-            window.localStorage.setItem('theme', 'dark')
-            document.documentElement.classList.add('dark')
-        } else {
-            window.localStorage.setItem('theme', 'light')
-            document.documentElement.classList.remove('dark')
+        if (mode !== null) {
+            window.localStorage.setItem('theme', mode);
+            if (mode === 'dark') {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
         }
-    }, [mode])
+    }, [mode]);
 
     return [mode, setMode] as const;
-}
+};
 
-export default useThemeSwitcher
+export default useThemeSwitcher;
