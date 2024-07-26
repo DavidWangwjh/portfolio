fc38f93c959d9b79673d23292907875ce68b9424
DavidWangwjh <davidwang.wjh@gmail.com>
2024-07-25 20:05:24 -0700

    updated licon

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -13,8 +13,8 @@ const LiIcon = ({reference}) => {
         <figure className='absolute -left-11 lg:left-0 stroke-dark dark:stroke-light'>
             <svg width='75' height='75' viewBox='0 0 100 100' className='-rotate-90 w-[60px] md:w-[75px]'>
                 <circle cx='75' cy='50' r='20' className='stroke-primary dark:stroke-primaryDark stroke-1 fill-none'/>
-                <motion.circle cx='75' cy='50' r='20' className='stroke-[5px] fill-light dark:fill-dark' style={{ pathLength: scrollYProgress}}/>
-                <circle cx='75' cy='50' r='10' className='animate-pulse stroke-1 fill-dark dark:fill-light'/>
+                {/* <motion.circle cx='75' cy='50' r='20' className='stroke-[5px] fill-light dark:fill-dark' style={{ pathLength: scrollYProgress}}/> */}
+                <circle cx='75' cy='50' r='14' className='animate-scale stroke-1 fill-dark dark:fill-light'/>
             </svg>
         </figure>
     )
